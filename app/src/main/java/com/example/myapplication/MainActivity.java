1867fdedd867954f4869dc043ae2a96215810c4e
taira_akira <taira_akira@nago_etech.local>
2020-04-27 13:03:39 +0900

    20200427

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registerd accelerometer listener");
+
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Y: " + sensorEvent.values[2]);
+
+        textView2.setText("onSensorChanged: X: " + sensorEvent.values[0]);
+        textView3.setText("onSensorChanged: Y: " + sensorEvent.values[1]);
+        textView4.setText("onSensorChanged: Z: " + sensorEvent.values[2]);
+    }
+}
